3c5378e01bf4acaee3645514ca68c41b7498290c
Eduardo Kuwakino <kuwakino@gmail.com>
2021-12-26 13:44:41 -0300

    feat(operations) - controller - addition and subtraction;

---
+++ ArithmeticAPI/Controllers/OperationsController.cs
@@ -0,0 +1,32 @@
+﻿using ArithmeticAPI.Services;
+using ArithmeticAPI.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArithmeticAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class OperationsController : Controller
+    {
+        private SumService sumService;
+
+        public OperationsController()
+        {
+            sumService = new SumService();
+        }
+
+        [HttpPost("/addition")]
+        public int Addition(int[] values)
+        {
+            var usecase = new AdditionUseCase(sumService);
+            return usecase.Add(values);
+        }
+
+        [HttpPost("/subtraction")]
+        public int Subtraction(int[] values)
+        {
+            var usecase = new SubtractionUseCase(sumService);
+            return usecase.Subtract(values);
+        }
+    }
+}
